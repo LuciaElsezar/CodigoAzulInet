f2e46501dadba9ec415b877bbf0e169e2fd375e2
Lucía Elsezar <luciaelsezaravila@gmail.com>
2023-10-01 15:33:58 -0300

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/footer/footer.component.css 	new file:   src/app/footer/footer.component.html 	new file:   src/app/footer/footer.component.spec.ts 	new file:   src/app/footer/footer.component.ts 	new file:   src/app/index/index.component.css 	new file:   src/app/index/index.component.html 	new file:   src/app/index/index.component.spec.ts 	new file:   src/app/index/index.component.ts 	new file:   src/app/log-in/log-in.component.css 	new file:   src/app/log-in/log-in.component.html 	new file:   src/app/log-in/log-in.component.spec.ts 	new file:   src/app/log-in/log-in.component.ts 	new file:   src/app/navbar/navbar.component.css 	new file:   src/app/navbar/navbar.component.html 	new file:   src/app/navbar/navbar.component.spec.ts 	new file:   src/app/navbar/navbar.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule, provideRouter, withComponentInputBinding } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { IndexComponent } from './index/index.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+const routes:Routes = [
+  {path:'log-in', component:LogInComponent},
+  {path:'', component:IndexComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    IndexComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppModule { }
---
+++ src/app/log-in/log-in.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.css']
+})
+export class LogInComponent {
+  formularioLogIn = this.fb.group({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    mantenerSesion: ['']
+  });
+
+  adminLogIn(){
+    if(this.formularioLogIn.valid){
+      if(this.formularioLogIn.controls['email'].value == "escuela713@gmail.com" &&
+      this.formularioLogIn.controls['password'].value == "12345678"){
+        this.router.navigateByUrl("");
+      }
+    }
+  }
+  constructor(private fb:FormBuilder, private router:Router) {}
+}
